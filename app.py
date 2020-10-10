0f3cd47604d7efde285c2bdc0fe09fafc3d5114a
MSWLandi <38134398+mswlandi@users.noreply.github.com>
2020-10-10 17:40:18 -0300

    Login and Register using the database

--- app.py
+++ app.py
@@ -13,29 +13,43 @@ session_scope = db.get_session_scope()
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        username = request.form["username"]
-        password = request.form["password"]
-        email = request.form["email"]
+        postType = request.form["type"]
 
-        print("username:" + username)
-        print("senha:" + password)
-        print("email:" + email)
-
-        with session_scope() as session:
-            user = Users(name=username, email=email, password=password)
-
-            session.add(user)
-
-            session.commit()
-
-        return redirect('/login-feito')
+        if (postType == "register"):
+            return register(request.form)
+        elif (postType == "login"):
+            return login(request.form)
     else:
-        return render_template('index.html', nome="Marcos")
+        return render_template('index.html')
 
-@app.route('/login-feito', methods=["GET"])
-def login_feito():
-    return render_template('login-feito.html')
+def register(form):
+    username = form["username"]
+    password = form["password"]
+    email = form["email"]
 
+    with session_scope() as session:
+        nomeQuery = session.query(Users).filter_by(name=username).first()
+        if (nomeQuery):
+            print("Usuario ja existe")
+            return render_template('index.html', error_register="Usuário já existe")
+        else:
+            user = Users(name=username, email=email, password=password)
+            session.add(user)
+            session.commit()
+            return render_template('register_success.html')
+
+
+def login(form):
+    username = form["username"]
+    password = form["password"]
+    email = form["email"]
+
+    with session_scope() as session:
+        nomeQuery = session.query(Users).filter_by(name=username, email=email, password=password).first()
+        if (nomeQuery):
+            return render_template('login_success.html')
+        else:
+            return render_template('index.html', error_login="Usuário inexistente")
 
 if __name__ == '__main__':
     app.run()
